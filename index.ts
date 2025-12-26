84ecdec855e118acdfdd0f0604e4d132c24c131a
serhiibudianskyi <serhiibudianskyi@gmail.com>
2025-12-26 13:14:57 +0200

    feat: create index file for component exports and update tsconfig to include it

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './src/types/Field';
+export * from './src/types/Form';
+export * from './src/components/Field';
+export * from './src/components/FileField';
+export * from './src/components/Form';
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types/Field';
-export * from './types/Form';
-export * from './components/Field';
-export * from './components/FileField';
-export * from './components/Form';
